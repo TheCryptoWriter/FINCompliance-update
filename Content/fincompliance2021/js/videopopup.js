639f0faf4e2d73189332efc0080f538b51dfcd2b
davidmcneal28 <davidmcneal28@gmail.com>
2021-11-18 04:49:23 -0700

    --- Auto-Git Commit ---

--- Content/fincompliance2021/js/videopopup.js
+++ Content/fincompliance2021/js/videopopup.js
@@ -48,4 +48,18 @@ $(document).ready(function () {
     });
   });
 });
+$("#moreinfo").click(function (event) {
+  event.preventDefault();
+  this.blur(); // Manually remove focus from clicked link.
+  $.get(this.href, function (html) {
+    $(html).appendTo("body").modal();
+  });
+  var html;
+  if (html) {
+    html.appendTo("body");
+    html = null;
+  } else {
+    html = $(".modal").detach();
+  }
+});
 // document ready
